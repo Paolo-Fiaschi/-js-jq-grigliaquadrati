68966c982bed18423bd848bc43ec115a3f825f9d
Paolo <p.fiaschi@hotmail.it>
2020-04-03 18:56:23 +0200

    aggiunta 64 div random e class redwannabe(da correggere)

--- main.js
+++ main.js
@@ -24,14 +24,34 @@ var redPoint = 0, greenPoint = 0;
   //   }
   // );
 
+  for (var i = 0; i < 64; i++) {
+    var random = Math.floor(Math.random() * 64);
+    var divTableConteiner = $(".tablecontainer");
+    var divRandom = $("div .tablecontainer").append('<div class="square"></div>');
 
+  }
   // ciclo per aggiungere 15 volete redWannabe in maniera randomica a square
   for (var i = 0; i < 15; i++) {
 
     var random = Math.floor(Math.random() * 64);
     var square = $(".square");
+    console.log(random);
 
-    square.eq(random % square.length).addClass('redWannabe')[i]++;
+    //square.eq(random % square.length).addClass('redWannabe')[i]++;
+    if ($(".tablecontainer").find(square).eq(random).hasClass('redWannabe')) {
+      $(this).removeClass('redWannabe');
+
+      console.log(random);
+
+
+    }else{
+      $(".tablecontainer").find(square).eq(random).addClass('redWannabe');
+
+    }
+    // estraggo un numero
+    // verifico se il quadrato collegato a quel numero non ha redWannabe
+    // se non ha redWannabe gliel'assegno e vado al prossio ciclo for
+    // se ha già redWannabe devo generare un altro numero random
 
   }
   console.log(square);
